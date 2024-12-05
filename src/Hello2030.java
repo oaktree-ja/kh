daa477757d7e6e4861c2e2e1553ab64a29072442
user1 <user1@DESKTOP-JS3EKPF>
2024-12-05 17:40:10 +0900

    [ADD] Read me file
    day 01 file
    hello 2030

---
+++ src/Hello2030.java
@@ -0,0 +1,12 @@
+
+public class Hello2030 {
+	public static void main(String [] args) {
+		int year = 2030;
+		double second = 11.18;
+		String name = "트럼프";
+		char mbti = 'T';
+		boolean result = true;
+		// 숫자(정수/실수), 문자(문자/문자열), 참/거짓 
+		System.out.println("헬로 " + year);
+	}
+}
---
+++ src/day01/basic/README.md
@@ -0,0 +1,13 @@
+# 자바 기초
+- 자바는 객체지향
+- 자바는 WORA
+- 바이트 코드
+- JVM
+```java
+public class HelloJava {
+	public static void main(String [] args) {
+		System.out.println("Hello Java");
+	}
+}
+```
+## 변수 자료형 상수
